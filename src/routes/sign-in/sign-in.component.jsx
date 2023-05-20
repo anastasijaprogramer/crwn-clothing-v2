5c141acb73f8ab923d0b77753e58017fb8667ab1
anastasija radulovic <anastasija.radulovic@tasteatlas.com>
2023-05-20 11:30:52 +0200

    add signIn file

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,8 +1,19 @@
+import { signInWithGooglePopup } from "../../utils/firebase/firebase.utils.js";
+
 
 const SignIn = () => {
+    //whenver we make a request to databse we will use asynchronous functions
+    const logGoogleUser = async () => {
+        const response = await signInWithGooglePopup();
+        console.log(response);
+    }
+
     return (
         <div>
             <h1>Sign in page</h1>
+            <button onClick={logGoogleUser}>
+                Sign in with Google popup
+            </button>
         </div>
     );
 }
